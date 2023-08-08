870964ae141f70578155ca40ad60d129fed43beb
John Garcia <jgarcia3788@yahoo.co.uk>
2023-08-08 13:10:40 +0100

    added ssh

--- custom.nix
+++ custom.nix
@@ -10,5 +10,14 @@
     pkgs.git
     # Add more packages here as needed
   ];
+    users.users.nixos = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" ];
+    initialHashedPassword = "";
+    openssh.authorizedKeys.keys = [ "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBDj2FdHqTEFS2AFwVXbc/93v+tKlD5MlSOFwWlGAJoNVFuOZh0sptdnaDR1XwIFCfGtFGvx0vNHJxe8uIFUbP0=" ];
+  };
+
+  
 }
 
